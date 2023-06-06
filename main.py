c8c19e2dc4c41d72fd7478ba0a6101c81f5a7e86
ammicco <ngrecchi1@gmail.com>
2023-06-06 10:22:11 +0200

    fix some bugs

--- main.py
+++ main.py
@@ -2,7 +2,20 @@
 
 import requests, sys, getopt, json, hashlib
 
-__MAXLEN__ = 60 # if response lenght is higher will be printed its hash 
+__MAXLEN__ = 120 # if response lenght is higher will be printed its hash 
+
+__MHEAD__ = [ # must have security headers
+    "Content-Security-Policy",
+    "X-Frame-Options", 
+    "X-Content-Type-Options",
+    "Referrer-Policy",
+    "Permissions-Policy"
+]
+
+__NHCOOKIE__ = [ # interesting cookie names
+    "id", 
+    "sessionId"
+]
 
 def __usage(): # function to print an help message
     print("""Usage: python3 main.py <command> <-f apis-file> [options] <hostname>
@@ -49,19 +62,19 @@ def __brute_force(apis): # function to combine every word of the wordlist with a
 
     return comp_apis
 
-def __scan(proto, hostname, apis, verbose, exclude, printRes, body, follow, header, cookie): # function to send request to all the endpoints
-    print() # just to leave some space
+def __scan(proto, method, hostname, apis, verbose, exclude, printRes, body, follow, header, cookie): # function to send request to all the endpoints
     out = {} # to create output 
     headers = {} # to store req headers
     cookies ={} # to store req cookies
     r = {} # to store temp output data
-    warn = [] # to store warnings
 
-    if header != "": # if pass some headers as argument update the dict 
+    if header != "": # if pass some headers or cookie as argument update the dict 
         headers.update(header)
 
     if cookies != "":
         cookies.update(cookie)
+
+    print() # just to leave some space
 
     for end in apis: # for all the endpoints
         url = f"{proto}://{hostname}/{end}" # create the url with protocol (http or https), hostname and endpoint name
@@ -69,163 +82,150 @@ def __scan(proto, hostname, apis, verbose, exclude, printRes, body, follow, head
         out[url] = {} # create an entry in the results dict named as the url
 
         for met in method: # for all the method specified  
-            if body[0] == "json" and met == "POST": # if the request is a POST with json payload 
-                headers["content-type"] = "application/json" # set the appropriate header
-                response = requests.request(met, url = url, json = body[1], allow_redirects = follow, headers = headers, cookies = cookies) # send the request
-            elif body[0] == "urlenc" and met == "POST": # if the req is a POST with urlencoded data
-                headers["content-type"] = "application/x-www-form-urlencoded" # set the header 
-                response = requests.request(met, url = url, data = body[1], allow_redirects = follow, headers = headers, cookies = cookie) # send the request
+            hwarn = [] # to store warnings
+            cwarn = []
+            
+            if met == "POST":
+                if body[0] == "json":
+                    headers["content-type"] = "application/json" # set the appropriate header
+                    response = requests.request(met, url = url, json = body[1], allow_redirects = follow, headers = headers, cookies = cookies) # send the request
+                elif body[0] == "urlenc": 
+                    headers["content-type"] = "application/x-www-form-urlencoded" # set the header 
+                    response = requests.request(met, url = url, data = body[1], allow_redirects = follow, headers = headers, cookies = cookie) # send the request
             else:
                 response = requests.request(met, url = url, allow_redirects = follow, headers = headers, cookies = cookie) # if there's no payload send the request
 
             # if the response code is a 200's family code or verbose is set and code is different from the one to exclude
             if ((response.status_code > 199 and response.status_code < 300) or (verbose is True and response.status_code != 404)) and (response.status_code != int(exclude)):
-                if len(response.text) > __MAXLEN__: # if the response text len is higher than the limit
+                if len(response.text) > __MAXLEN__ and printRes == False: # if the response text len is higher than the limit
                     text = hashlib.md5(response.text.encode()).hexdigest() # hash the response
                 else:
                     text = response.text # else save the plain response
 
-                print(f"[ \033[92m OK \033[0m ] - \033[1mUrl\033[0m: \033[96m{url}\033[0m, \
-\033[1mmethod\033[0m: \033[91m{met}\033[0m, \
-\033[1mstatus\033[0m: \033[93m{response.status_code}\033[0m, \
-\033[1mresponse\033[0m: {text}\n") # print formatted output for the response 
+                # print formatted output for the response 
+                print(f"[ \033[92m OK \033[0m ] - \033[1mUrl\033[0m: \033[96m{url}\033[0m, \033[1mmethod\033[0m: \033[91m{met}\033[0m, \033[1mstatus\033[0m: \033[93m{response.status_code}\033[0m, \033[1mresponse\033[0m: {text}\n")
 
                 r["status"] = response.status_code # insert the status code
-
-                if printRes is True: # insert response text 
-                    r["response"] = response.text
-                else:
-                    r["response"] = text
-
+                r["response"] = text # insert the response text
                 r["headers"] = dict(response.headers) # insert response headers
 
-                for h in mhead: # check if must have headers isnt set in the response
+                for h in __MHEAD__: # check if must have headers isnt set in the response
                     if h not in response.headers:
-                        warn.append(f"header {h} not set!") # if isnt set make a warning
+                        hwarn.append(f"header {h} not set!") # if isnt set make a warning
 
-                if len(warn) != 0: # if there's some warning add it to the output
-                    r["headers"]["header-warnings"] = warn
+                if len(hwarn) != 0: # if there's some warning add it to the output
+                    r["headers"]["header-warnings"] = hwarn
 
                 if response.cookies != {}: # insert the cookies
                     r["cookies"] = dict(response.cookies)
 
-                warn.clear()
-
-                for c in nhcookie:
+                for c in __NHCOOKIE__: # check if there's some interesting cookies in the response
                     if c in response.cookies:
-                        warn.append(f"found a {c} cookie!")
+                        cwarn.append(f"found a {c} cookie!")
 
-                if len(warn) != 0: 
-                    r["cookies"]["cookie-warnings"] = warn
+                if len(cwarn) != 0: 
+                    r["cookies"]["cookie-warnings"] = cwarn
                 
                 out[url][met] = r # add r to the output
 
-        if not out[url]:
+        if not out[url]: # if the endpoint doesnt responde, not include in the output
             out.pop(url)
             
     return out
 
-proto = "https"
-hostFile = apiFile = outFile = header = ""
+def __main(): # main function
+    proto = "https" # default protocol is https
+    hostFile = apiFile = outFile = header = "" # some stuff
+    
+    verbose = printRes = follow = False
 
-verbose = printRes = follow = False
+    exclude = 0
 
-exclude = 0
+    cookies = {}
 
-cookies = {}
+    method = ["GET", "POST"] # default methods are GET and POST
+    output = []
+    body = ["", ""]
 
-method = ["GET", "POST"]
-output = []
-body = ["", ""]
+    if ("-h" in sys.argv or "--help" in sys.argv) or ("-f" not in sys.argv and "--file" not in sys.argv) or (sys.argv[1] != "scan" and sys.argv[1] != "force"): # if -h is in the args or
+        __usage()                                                                                                                                               # if -f not in arsg or
+        exit()                                                                                                                                                  # if argv[1] isnt the command, display help
 
-mhead = [
-    "Content-Security-Policy",
-    "X-Frame-Options", 
-    "X-Content-Type-Options",
-    "Referrer-Policy",
-    "Permissions-Policy"
-]
-
-nhcookie = [
-    "id", 
-    "sessionId"
-]
-
-if sys.argv[1] == "-h":
-    start = 1
-else:
-    start = 2
-
-try:
-    opts, args = getopt.getopt(sys.argv[start:], "l:p:o:f:m:j:d:H:c:x:Frvh", ["list=", "protocol=", "output=", "file=", "method=", "json=", "data=", "header=", "cookies=", "exclude=", "follow", "reponse", "verbose", "help"])
-except getopt.GetoptError as err:
-    print(err) 
-    __usage()
-    exit()
-
-if len(args) > 0:
-    hostname = args[0]
-
-for opt, arg in opts:
-    if opt in ["-l", "--list"]:
-        hostFile = arg
-    if opt in ["-p", "--protocol"]:
-        proto = arg
-    if opt in ["-o", "--output"]:
-        outFile = arg
-    if opt in ["-f", "--file"]:
-        apiFile = arg
-    if opt in ["-m", "--method"]:
-        mets = arg
-        method.clear()
-
-        if "," in mets:
-            mets = mets.split(",")
-
-            for m in mets:
-                method.append(m)
-        else:
-            method.append(mets)
-    if opt in ["-j", "--json"]:
-        method.pop(0)
-        body[0] = "json"
-        body[1] = json.loads(arg)  
-    if opt in ["-d", "--data"]:
-        method.pop(0)
-        body[0] = "urlenc"
-        body[1] = arg 
-    if opt in ["-H", "--header"]:
-        header = json.loads(arg)
-    if opt in ["-c", "--cookie"]:
-        cookies = json.loads(arg)
-    if opt in ["-F", "--follow"]:
-        follow = True   
-    if opt in ["-x", "--exclude"]:  
-        exclude = arg
-    if opt in ["-r", "--reponse"]:
-        printRes = True
-    if opt in ["-v", "--verbose"]:
-        verbose = True
-    if opt in ["-h", "--help"]:
+    # parse arguments
+    try:
+        opts, args = getopt.getopt(sys.argv[2:], 
+        "l:p:o:f:m:j:d:H:c:x:Frvh", 
+        ["list=", "protocol=", "output=", "file=", "method=", "json=", "data=", "header=", "cookies=", "exclude=", "follow", "reponse", "verbose", "help"])
+    except getopt.GetoptError as err:
+        print(err) 
         __usage()
         exit()
 
-command = sys.argv[1]
+    if len(args) > 0:
+        hostname = args[0]
 
-apis = __parse(apiFile)
+    for opt, arg in opts:
+        if opt in ["-l", "--list"]:
+            hostFile = arg
+        elif opt in ["-p", "--protocol"]:
+            proto = arg
+        elif opt in ["-o", "--output"]:
+            outFile = arg
+        elif opt in ["-f", "--file"]:
+            apiFile = arg
+        elif opt in ["-m", "--method"]:
+            mets = arg
+            method.clear()
 
-if command == "force":
-    apis = __brute_force(apis)
+            if "," in mets:
+                mets = mets.split(",")
 
-if hostFile != "":
-    for hostname in open(hostFile, "r"):
-        output.append(__scan(proto, hostname.replace("\n", ""), apis, verbose, exclude, printRes, body, follow, header, cookies))
-else:
-    output.append(__scan(proto, hostname, apis, verbose, exclude, printRes, body, follow, header, cookies))
+                for m in mets:
+                    method.append(m)
+            else:
+                method.append(mets)
+        elif opt in ["-j", "--json"]:
+            method = ["POST"]
+            body[0] = "json"
+            body[1] = json.loads(arg)  
+        elif opt in ["-d", "--data"]:
+            method = ["POST"]
+            body[0] = "urlenc"
+            body[1] = arg 
+        elif opt in ["-H", "--header"]:
+            header = json.loads(arg)
+        elif opt in ["-c", "--cookie"]:
+            cookies = json.loads(arg)
+        elif opt in ["-F", "--follow"]:
+            follow = True   
+        elif opt in ["-x", "--exclude"]:  
+            exclude = arg
+        elif opt in ["-r", "--reponse"]:
+            printRes = True
+        elif opt in ["-v", "--verbose"]:
+            verbose = True
+        elif opt in ["-h", "--help"]:
+            __usage()
+            exit()
 
-if outFile != "":
-    of = open(outFile, "w")
+    command = sys.argv[1]
 
-    of.write(json.dumps(output, indent = 2))
+    apis = __parse(apiFile) # get apis list
 
-    of.close()
+    if command == "force": # if the command is force, create the endpoints
+        apis = __brute_force(apis)
+
+    if hostFile != "": # if there's a hostnames file, scan all of them
+        for hostname in open(hostFile, "r"):
+            output.append(__scan(proto, method, hostname.replace("\n", ""), apis, verbose, exclude, printRes, body, follow, header, cookies))
+    else: # else scan the only one
+        output.append(__scan(proto, method, hostname, apis, verbose, exclude, printRes, body, follow, header, cookies))
+
+    if outFile != "": # if indicate an output file write the output on this
+        of = open(outFile, "w")
+        
+        of.write(json.dumps(output, indent = 2))
+
+        of.close()
+
+__main() # start
